06e251bcf6c6f5937c0b9a122bb1e9a845f377a5
Matheus Silva <matthyblue@gmail.com>
2026-02-04 21:13:45 -0300

    Comments adjusts

--- application/sped_processor.py
+++ application/sped_processor.py
@@ -2,7 +2,6 @@ import time
 from application.sped_errors import SpedErrors
 
 def parse_float(value):
-    """ Converte string para float, tratando valores vazios """
     try:
         return float(value.replace(",", ".")) if value.strip() else 0.0
     except ValueError:
@@ -11,8 +10,8 @@ def parse_float(value):
 class SpedProcessor:
     def validate_sped(self, file_path, progress_signal):
         errors = SpedErrors()
-        vl_doc_map = {}  # Dicionário para armazenar VL_DOC por número de documento
-        last_num_doc = None  # Variável para armazenar o último número de documento C100
+        vl_doc_map = {} 
+        last_num_doc = None 
         
         with open(file_path, 'r', encoding='utf-8-sig', errors='replace') as file:
             lines = file.readlines()
@@ -22,20 +21,20 @@ class SpedProcessor:
                 parts = line.strip().split('|')
                 
                 if len(parts) > 12 and parts[1] == "C100":
-                    last_num_doc = parts[8]  # Número do documento
+                    last_num_doc = parts[8]  
                     vl_doc = parse_float(parts[12])
                     vl_doc_map[last_num_doc] = vl_doc  # Armazena VL_DOC com o número do documento
                 
                 if len(parts) > 5 and parts[1] == "C190" and last_num_doc:
-                    vl_opr = parse_float(parts[5])  # Corrigido índice para 5
+                    vl_opr = parse_float(parts[5])  
                     
                     if last_num_doc in vl_doc_map and vl_doc_map[last_num_doc] != vl_opr:
                         errors.add_error(f"Divergência encontrada no doc {last_num_doc}: VL_DOC ({vl_doc_map[last_num_doc]}) ≠ VL_OPR ({vl_opr})")
                 
-                # Atualiza a barra de progresso
+                # Barra de progresso
                 percent = int(((i + 1) / total_lines) * 100)
                 progress_signal.emit(percent)
                 
-                time.sleep(0.01)  # Simula o processamento
+                time.sleep(0.01)  
         
         return errors.get_errors()
